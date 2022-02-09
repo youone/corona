85af5b2107a33ea3de6b361b6d4abdc859741547
Johan Lundquist <jolundq@gmail.com>
2022-02-09 12:43:18 +0100

    wip

--- src/forum/forum_backup.js
+++ src/forum/forum_backup.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const wget = require('node-wget');
 const {execSync} = require("child_process");
 
 function wait(ms)
@@ -9,9 +10,15 @@ function wait(ms)
     while(d2-d < ms);
 }
 
-// for (let i=0; i<=3885; i+=15) {
-for (let i=3765; i<=292*15; i+=15) {
-    console.log(i);
-    execSync(`wget --no-check-certificate 'https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}'`);
+for (let i=336*15; i<=336*15; i+=15) {
+    console.log(`https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}`);
+    wget({url: `https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}`, dest: `./start_${i}.html`})
     wait(2000)
 }
+// for (let i=4395; i<=336*15; i+=15) {
+//     console.log(`https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}`);
+//     // execSync(`wget --no-check-certificate 'https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}'`);
+//     // wget(`https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}`);
+//     wget({url: `https://forum.vof.se/viewtopic.php?f=10&t=32806&start=${i}`, dest: `./start_${i}.html`});
+//     wait(2000)
+// }
